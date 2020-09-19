33d7c89f4cd1b113f6796e0bb29ca68fe98302ef
michaelcraigWeber <47675916+michaelcraigWeber@users.noreply.github.com>
2020-09-19 17:20:33 -0600

    John Lemon's Haunted Jaunt: Part 3
    Completed tutorial sections 7-10, built the game, and compiled it into an installer

--- John Lemon's Haunted Jaunt/Assets/Scripts/PlayerMovement.cs
+++ John Lemon's Haunted Jaunt/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
 
     Animator m_Animator;
     Rigidbody m_Rigidbody;
+    AudioSource m_audioSource;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
     
@@ -16,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
     {
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -31,6 +33,18 @@ public class PlayerMovement : MonoBehaviour
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool("IsWalking", isWalking);
+
+        if (isWalking)
+        {
+            if (!m_audioSource.isPlaying)
+            {
+                m_audioSource.Play();
+            }
+        }
+        else
+        {
+            m_audioSource.Stop();
+        }
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
